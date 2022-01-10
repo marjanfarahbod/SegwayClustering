261e2eab69cefee147f06fd445c33790604c42c3
marjanfarahbod <marjan.farahbod@gmail.com>
2022-01-10 11:16:23 -0800

    adding all files to

--- util/get_annotation.py
+++ util/get_annotation.py
@@ -1,1 +1,17 @@
 # in this code I will get the annotations from the .bed file 
+
+####################
+# inputs
+####################
+
+# path to the mnemonics file
+
+
+# path to the .bed file
+
+####################
+# create the output
+####################
+
+
+
