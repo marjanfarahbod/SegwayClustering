e6bc3dcacbd28dac1b3ef28512e3475566d38704
marjanfarahbod <marjan.farahbod@gmail.com>
2023-02-21 13:25:28 -0800

    get_sample_tissueClass.py added

---
+++ get_sample_tissueClass.py
@@ -0,0 +1,1 @@
+# I want to have some stat plots for count of labels and length distributions in different tissue categories. Questions I want answer to: are some labels specific to a tissue_category? For example, bivalent, k9k36 or sth else. 
